bf914510ce643088368afe1d4ce60ac7ab541c01
muhammedbozkurt <muhammedbozkurrt@outlook.com>
2022-10-26 05:26:33 +0300

    admin and ui models & components created

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { OrderModule } from './order/order.module';
+import { CustomerModule } from './customer/customer.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    OrderModule,
+    CustomerModule
+  ]
+})
+export class ComponentsModule { }
