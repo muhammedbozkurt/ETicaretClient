bf4b23602d99d08e56f1ef47a16f272b2f6f04cf
muhammedbozkurt <muhammedbozkurrt@outlook.com>
2022-11-01 02:11:00 +0300

    Ngx-Spinner added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,17 @@ import { LayoutComponent } from './admin/layout/layout.component';
 import { HomeComponent } from './ui/components/home/home.component';
 
 const routes: Routes = [
-  {path:"admin", component: LayoutComponent, children: [
-    {path: "", component: DashboardComponent},
-    {path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule)},
-    {path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule)},
-    {path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule)}
-  ]},
-  {path: "", component: HomeComponent},
-  {path: "baskets", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule)},
-  {path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule)}
+  {
+    path: "admin", component: LayoutComponent, children: [
+      { path: "", component: DashboardComponent },
+      { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule) },
+      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) },
+      { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule) }
+    ]
+  },
+  { path: "", component: HomeComponent },
+  { path: "baskets", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
+  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) }
 ];
 
 @NgModule({
