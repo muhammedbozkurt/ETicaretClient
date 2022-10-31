10b6307c534dde7fff89f97a1ea1481ca01f20ab
muhammedbozkurt <muhammedbozkurrt@outlook.com>
2022-11-01 01:18:59 +0300

    Toastr library added for ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -8,4 +9,7 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  constructor(private toastrService: CustomToastrService) {
+    toastrService.message("selam", "başlık", { messageType: ToastrMessageType.Error, position: ToastrPosition.BottomLeft });
+  }
 }
